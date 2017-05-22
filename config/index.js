c0cfa02561047a9dceeee21284c2b3982f808ed5
fucuicui <1280073025@qq.com>
2017-05-22 14:50:32 +0800

    commit

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
     env: {
       NODE_ENV: '"development"'
     },
-    port: 8000,
+    port: 9000,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     context: [ //代理路径
